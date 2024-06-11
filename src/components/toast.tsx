43e593a24083c92fe9697cf1ec012f89d3dc3ef0
spaaacetoast <spaaacetoast@proton.me>
2024-06-11 11:12:12 +0200

    Initial commit

---
+++ src/components/toast.tsx
@@ -0,0 +1,35 @@
+import { Toast, Toaster, createToaster } from '@ark-ui/solid';
+
+export const Basic = () => {
+  const toaster = createToaster({
+    id: 'test',
+    placement: 'bottom-end',
+    gap: 24,
+  });
+
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={() =>
+          toaster.create({
+            title: 'Loading!',
+            description: 'We are loading something for you. Please wait.',
+            type: 'info',
+          })
+        }
+      >
+        Add Toast
+      </button>
+      <Toaster toaster={toaster}>
+        {(toast) => (
+          <Toast.Root>
+            <Toast.Title>{toast().title}</Toast.Title>
+            <Toast.Description>{toast().description}</Toast.Description>
+            <Toast.CloseTrigger>{/* <XIcon /> */}X</Toast.CloseTrigger>
+          </Toast.Root>
+        )}
+      </Toaster>
+    </div>
+  );
+};
---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { Title } from '@solidjs/meta';
+import { Avatar } from '~/components/avatar';
+import { Basic } from '~/components/toast';
+
+export default function Home() {
+  return (
+    <main>
+      <Title>Welcome to Ark UI</Title>
+      <h1>Welcome to Ark UI</h1>
+      <Basic />
+    </main>
+  );
+}
